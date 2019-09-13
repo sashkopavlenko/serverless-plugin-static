15b9838a9dbfd494e43a936c652b1010583d6f87
sashko <7355623+a-pavlenko@users.noreply.github.com>
2019-09-13 23:03:45 +0300

    basic static files serving

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # serverless-plugin-static
+
+[![Build Status](https://travis-ci.com/a-pavlenko/serverless-plugin-static.svg?branch=master)](https://travis-ci.com/a-pavlenko/serverless-plugin-static)
+
 A serverless framework plugin to serve static files locally
---
+++ src/serve.ts
@@ -0,0 +1,62 @@
+import * as http from 'http';
+import * as url from 'url';
+import * as path from 'path';
+import { promises as fs } from 'fs';
+import * as Serverless from 'serverless';
+
+interface contentTypeMap {
+  readonly [key: string]: string;
+}
+
+const fileExtToContentTypeMap: contentTypeMap = {
+  '.ico': 'image/x-icon',
+  '.html': 'text/html',
+  '.js': 'text/javascript',
+  '.json': 'application/json',
+  '.css': 'text/css',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.wav': 'audio/wav',
+  '.mp3': 'audio/mpeg',
+  '.svg': 'image/svg+xml',
+  '.pdf': 'application/pdf',
+  '.doc': 'application/msword',
+};
+
+const handler = (folder: string) => async (
+  req: http.IncomingMessage,
+  res: http.ServerResponse
+) => {
+  try {
+    const parsedUrl = url.parse(req.url || '');
+    const pathname = path.join(folder, parsedUrl.pathname || '');
+
+    const { ext } = path.parse(pathname);
+
+    const data = await fs.readFile(pathname);
+
+    res.setHeader('Content-type', fileExtToContentTypeMap[ext] || 'text/plain');
+    return res.end(data);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      res.statusCode = 404;
+      return res.end(`File not found`);
+    }
+
+    res.statusCode = 500;
+    return res.end(`Error getting the file: ${error}.`);
+  }
+};
+
+export default (
+  serverless: Serverless,
+  { folder, port }: { folder: string; port: number | string }
+) => {
+  const requestHandler = handler(folder);
+  http.createServer(requestHandler).listen(port);
+
+  serverless.cli.log(`[ Static ] serving static files from ${folder} folder`);
+  serverless.cli.log(
+    `[ Static ] serving static files on http://localhost:${port}`
+  );
+};
