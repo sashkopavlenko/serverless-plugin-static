4380f3ea76d212dce36f84fb95852ff4c5cd0548
sashko <7355623+a-pavlenko@users.noreply.github.com>
2019-09-14 13:06:27 +0300

    add server test suite

--- src/serve.spec.ts
+++ src/serve.spec.ts
@@ -1,7 +1,29 @@
+import { Server } from 'https';
+import * as request from 'supertest';
 import serve from './serve';
 
-describe('serve', () => {
-  it('should be defined', () => {
-    expect(serve).toBeDefined();
+import Serverless = require('serverless');
+const serverless = {
+  cli: { log: jest.fn() },
+} as any;
+
+describe('Server', () => {
+  let server: Server;
+
+  beforeAll(async () => {
+    server = (await serve(<Serverless>serverless, {
+      folder: 'static',
+      port: 8080,
+    })) as Server;
   });
+
+  it('should be defined', () => {
+    expect(server).toBeDefined();
+  });
+
+  it('GET /static/not-exists', done =>
+    request(server)
+      .get('/static/not-exists')
+      .expect(404)
+      .end(done));
 });
